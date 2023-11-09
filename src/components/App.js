d42cac3decaca6141870962f730215a128762a10
clementmwamba <clementmwamba23@gmail.com>
2023-11-09 12:25:43 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import ProjectList from "./ProjectList";
 import user from "../data/user";
-
+import ProjectList from "./ProjectList";
 function App() {
   return (
     <div>
@@ -12,6 +11,7 @@ function App() {
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
+     
     </div>
   );
 }
